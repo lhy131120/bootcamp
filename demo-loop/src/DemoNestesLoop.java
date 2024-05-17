e3ed5cb093233c5529dd5b820aef77a172a8fd2b
lhy <lhy131120@gmail.com>
2024-05-17 21:33:36 +0800

    demo-nestedloop

---
+++ demo-loop/src/DemoNestesLoop.java
@@ -0,0 +1,32 @@
+public class DemoNestesLoop {
+  public static void main(String[] args) {
+    // for (int i = 0; i < 4; i++) {
+    //   for (int j = 0; j < 2; j++) {
+    //     System.out.println("hello " + "i=" + i + ",j=" + j);
+    //   }
+    // }
+
+    for (int i = 0; i < 4; i++) {
+      if (i%2 == 0) continue;
+      for (int j = 0; j < 2; j++) {
+        if (j%2 != 0) continue;
+        System.out.println("hello " + "i=" + i + ",j=" + j);
+      }
+    }
+
+
+    // HomeWork
+    // 1-20 > 1,3,5,7,9,12,14,16,18,20
+
+    String s = "Vincent Book Game Laptop";
+    // Find the first index of charactor 'a'
+    // 14
+
+    for(int i=0; i<=s.length(); i++) {
+      if(s.charAt(i) == 'a') {
+        System.out.println(i);
+        break;
+      }
+    }
+  }
+}
